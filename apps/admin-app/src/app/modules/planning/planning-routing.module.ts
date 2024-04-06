2185ddc749f834f47d97e5d139793d53887b6605
Mordecai Godwin <mogodwin@axamansard.com>
2024-04-06 04:01:38 +0100

    creating board page

--- apps/admin-app/src/app/modules/planning/planning-routing.module.ts
+++ apps/admin-app/src/app/modules/planning/planning-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PlanningComponent } from "./planning.component";
 import { TasksComponent } from "./pages/tasks/tasks.component";
+import { BoardComponent } from "./pages/board/board.component";
 
 const routes: Routes = [
   {
@@ -9,7 +10,8 @@ const routes: Routes = [
     component: PlanningComponent,
     children: [
       {path: '', redirectTo: 'tasks', pathMatch: 'full'},
-      {path: 'tasks', component: TasksComponent},     
+      {path: 'tasks', component: TasksComponent},
+      {path: 'board', component: BoardComponent}
     ]
   }
 ];
