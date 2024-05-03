1907f96342230442879ab45b57d572c54a898ee1
Godwin Mordecai <davidire71@gmail.com>
2024-05-04 00:38:46 +0100

    Update environment.dev.ts to trigger Github Actions

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: false,
   env_name: 'Development',
   // apiUrl: 'https://localhost:7023/api/v1',
-  assetBaseUrl: 'https://localhost:7023/api/v1/assets/',
   apiUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1'
+  assetBaseUrl: 'https://localhost:7023/api/v1/assets/',
   // assetBaseUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1/assets/'
 };
