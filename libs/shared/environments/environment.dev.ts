e651fb328c9b8aa0919c960bc539d12e66a1d17d
Mordecai Godwin <davidire71@gmail.com>
2024-06-07 16:07:59 +0100

    updated

--- libs/shared/environments/environment.dev.ts
+++ libs/shared/environments/environment.dev.ts
@@ -2,8 +2,8 @@ export const environment = {
   staging: false,
   production: false,
   env_name: 'Development',
-  apiUrl: 'https://localhost:7023/api/v1',
-  assetBaseUrl: 'https://localhost:7023/api/v1/assets/',
-  // apiUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1'
-  // assetBaseUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1/assets/'
+  // apiUrl: 'https://localhost:7023/api/v1',
+  // assetBaseUrl: 'https://localhost:7023/api/v1/assets/',
+  apiUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1',
+  assetBaseUrl: 'https://saharaviewpoint-api.azurewebsites.net/api/v1/assets/'
 };
