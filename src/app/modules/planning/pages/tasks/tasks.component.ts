6b8be595379e875f4b6e54d94c18c794d1fc0226
Mordecai Godwin <mogodwin@axamansard.com>
2024-07-08 19:31:08 +0100

    feat: Add conditional rendering for "Change Due Date" button in tasks component

--- src/app/modules/planning/pages/tasks/tasks.component.ts
+++ src/app/modules/planning/pages/tasks/tasks.component.ts
@@ -50,7 +50,7 @@ import { SvpValidationErrorsComponent } from 'src/app/shared/components/input-fi
   animations: [
     trigger('toggleAnimation', [
       transition(':enter', [style({ opacity: 0, transform: 'scale(0.95)' }), animate('100ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))]),
-      transition(':leave', [animate('-75ms', style({ opacity: 0, transform: 'scale(0.95)' }))]),
+      transition(':leave', [animate('75ms', style({ opacity: 0, transform: 'scale(0.95)' }))]),
     ]),
   ],
 })
